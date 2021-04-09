ac9a7701ea53d46b7196143a543bd086b2b2ea1c
fakedance <72011267+fakedance@users.noreply.github.com>
2021-04-09 20:29:10 +0530

    Add project files.

---
+++ BlazorRealTime.Server/Controllers/NotificationController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorRealTime.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly IHubContext<NotificationHub> _hubContext;
+        public NotificationController(IHubContext<NotificationHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromQuery]string title)
+        {
+            await _hubContext.Clients.All.SendAsync("notification", $"{DateTime.Now}:{title}");
+            return Ok("Notification has has been sent successfully");
+        }
+    }
+}
